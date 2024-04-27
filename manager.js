38961416eaaa8c15b769ab706b2eac242f076f49
Kendra <cruickshank.kendra@gmail.com>
2024-04-27 11:53:16 -0700

    still on phase 4

--- manager.js
+++ manager.js
@@ -1,5 +1,7 @@
 const Employee = require("./employee.js")
 
+
+
 class Manager extends Employee{
     constructor(name, salary, title, manager, employees = []){
         super(name, salary, title, manager)
@@ -8,16 +10,41 @@ class Manager extends Employee{
     addEmployee(employee) {
         this.employees.push(employee)
     }
+    // helper function //recursive
+    _totalSubSalary(employees, sum = 0){
+       
+        if (employees.length === 0) {
+            return sum;
+        }
+        let employee = employees.shift();
+       
+        if (employee instanceof Manager){
+            sum += employee.salary; 
+            return _totalSubSalary(employees, sum)
+        }  
+        else {
+            sum += employee.salary;
+
+        }
+
+    }
+
+    //method
+    calculateBonus(multiplier){
+        return (this.salary + this._totalSubSalary(this.employees, 0)) * multiplier
+    }
 }
 
 const splinter = new Manager('Splinter', 100000, 'Sensei');
-console.log('Before: ', splinter);
+const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
+const raph = new Manager('Raphael', 90000, 'Ninja', leo);
+const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
+const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
 
-const leo = new Employee('Leonardo', 90000, 'Ninja', splinter);
-const mikey = new Employee('Michelangelo', 90000, 'Ninja', splinter);
-const donnie = new Employee('Donatello', 90000, 'Ninja', splinter);
-const raph = new Employee('Raphael', 90000, 'Ninja', splinter);
+console.log(splinter.calculateBonus(0.05)); // => 22500
+console.log(leo.calculateBonus(0.05)); // => 17500
+console.log(raph.calculateBonus(0.05)); // => 13000
 
-console.log('After: ', splinter);
+
 
 module.exports = Manager;
