df1519bfa92296d96838f651b6cc375d0ff815ee
kyle <averytkyle@gmail.com>
2024-04-27 16:45:54 -0400

    DONE

--- manager.js
+++ manager.js
@@ -11,40 +11,30 @@ class Manager extends Employee{
         this.employees.push(employee)
     }
     // helper function //recursive
-    _totalSubSalary(employees, sum = 0){
-       
-        if (employees.length === 0) {
+    _totalSubSalary() {
+        let sum = 0
+        if (this.employees.length === 0) {
             return sum;
         }
-        let employee = employees.shift();
-       
-        if (employee instanceof Manager){
-            sum += employee.salary; 
-            return _totalSubSalary(employees, sum)
-        }  
-        else {
-            sum += employee.salary;
 
+        for (let i = 0; i < this.employees.length; i++) {
+            let employee = this.employees[i]
+            if (employee instanceof Manager) {
+                sum += employee.salary;
+                this.employees.shift();
+                return sum + employee._totalSubSalary()
+            } else {
+                sum += employee.salary;
+            }
         }
-
+        return sum
     }
 
     //method
     calculateBonus(multiplier){
-        return (this.salary + this._totalSubSalary(this.employees, 0)) * multiplier
+        let totalSum = this._totalSubSalary()
+        return (this.salary + totalSum) * multiplier
     }
 }
-
-const splinter = new Manager('Splinter', 100000, 'Sensei');
-const leo = new Manager('Leonardo', 90000, 'Ninja', splinter);
-const raph = new Manager('Raphael', 90000, 'Ninja', leo);
-const mikey = new Employee('Michelangelo', 85000, 'Grasshopper', raph);
-const donnie = new Employee('Donatello', 85000, 'Grasshopper', raph);
-
-console.log(splinter.calculateBonus(0.05)); // => 22500
-console.log(leo.calculateBonus(0.05)); // => 17500
-console.log(raph.calculateBonus(0.05)); // => 13000
-
-
 
 module.exports = Manager;
